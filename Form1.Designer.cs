ad70ae82a9c24c6a3dab663022b4ab97a0d64412
Anatolii Martyniuk <Mart_kr93@student.itstep.org>
2023-08-30 17:45:42 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace SP_HW_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBox_FilePath = new System.Windows.Forms.TextBox();
+            this.txtBox_SearchValue = new System.Windows.Forms.TextBox();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите полный путь к файлу ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите значениие для поиска";
+            // 
+            // txtBox_FilePath
+            // 
+            this.txtBox_FilePath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBox_FilePath.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtBox_FilePath.Location = new System.Drawing.Point(13, 30);
+            this.txtBox_FilePath.Name = "txtBox_FilePath";
+            this.txtBox_FilePath.Size = new System.Drawing.Size(195, 23);
+            this.txtBox_FilePath.TabIndex = 2;
+            // 
+            // txtBox_SearchValue
+            // 
+            this.txtBox_SearchValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBox_SearchValue.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtBox_SearchValue.Location = new System.Drawing.Point(13, 76);
+            this.txtBox_SearchValue.Name = "txtBox_SearchValue";
+            this.txtBox_SearchValue.Size = new System.Drawing.Size(195, 23);
+            this.txtBox_SearchValue.TabIndex = 3;
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Search.Location = new System.Drawing.Point(15, 105);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(193, 44);
+            this.btn_Search.TabIndex = 4;
+            this.btn_Search.Text = "Найти";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(223, 160);
+            this.Controls.Add(this.btn_Search);
+            this.Controls.Add(this.txtBox_SearchValue);
+            this.Controls.Add(this.txtBox_FilePath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBox_FilePath;
+        private System.Windows.Forms.TextBox txtBox_SearchValue;
+        private System.Windows.Forms.Button btn_Search;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SP_HW_2
+{
+    public partial class Form1 : Form
+    {
+        private Thread thread;
+        public Form1() => InitializeComponent();
+        
+
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            thread = new Thread(new ThreadStart(Search));
+            thread.Start();
+        }
+        private void Search()
+        {
+            string path = txtBox_FilePath.Text.Trim('"');
+
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("Такого файла не существует", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtBox_FilePath.Text = string.Empty;
+                return;
+            }
+
+            if (txtBox_SearchValue.Text == string.Empty)
+            {
+                MessageBox.Show("Значение для поиска не задано", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int counter = 0;
+
+            foreach (var item in File.ReadAllText(path).Split(new char[] { ' ', ',', '.', '!', '?', ':', ';' }))
+                if (item.Contains(txtBox_SearchValue.Text))
+                    counter++;
+
+            MessageBox.Show($"Количество искомых символов/строк в файле: {counter}", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
